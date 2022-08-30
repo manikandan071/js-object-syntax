f9eb29597c1bd0c4de099aa3e682a9eabaf8e19f
manikandan <manidhiyamech07167@gmail.com>
2022-08-30 11:13:25 +0530

    js objects syntax

---
+++ js/main.js
@@ -0,0 +1,213 @@
+//1
+var mobile={
+	storage:"64gb",
+	camera:"64px",
+	display:"full screen",
+	price:10000
+}
+console.log(mobile);
+//2
+var watch={
+	type:"digital",
+	batterylife:"6months",
+	rawmaterial:"steel",
+	bluetooth:"no"
+}
+console.log(watch);
+
+//3
+var earphones={
+	brand:"boat",
+	color:"black",
+	version:"v5",
+	price:399
+}
+console.log(earphones);
+
+//4
+var tv={
+	brand:"onida",
+	screensize:"22 inch",
+	remotecontrol:"yes",
+	screentype:"led"
+}
+console.log(tv)
+
+//5
+var camera={
+	wifi:"available",
+	quality:"full HD",
+	brand:"canon",
+	type:"DSLR"
+}
+console.log(camera);
+
+//6
+var powerbank={
+	capacity:"10000mAh",
+	wattage:"30w",
+	cable:"type C",
+	weight:"500g"
+}
+console.log(powerbank);
+
+//7
+var mouse={
+	type:"wired",
+	color:"black",
+	size:"small",
+	price:300
+}
+console.log(mouse);
+
+//8
+var loan={
+	amount:30000,
+	intrest:"10%",
+	months:12
+}
+console.log(loan);
+
+//9
+var shoes={
+	brand:"puma",
+	color:"black",
+	size:8,
+	idealfor:"men"
+}
+console.log(shoes);
+
+//10
+var tshirt={
+	sleeve:"full sleeve",
+	fabric:"cotton",
+	size:"s",
+	fit:"regular"
+}
+console.log(tshirt);
+
+//11
+var cricketbat={
+	suitablefor:"leather ball",
+	weight:"1000g",
+	grade:"grade-1",
+	made:"english willow"
+}
+console.log(cricketbat);
+
+//12
+var cricketball={
+	material:"leather",
+	weight:"150g",
+	suitablefor:"mud pitch",
+}
+console.log(cricketball);
+
+//13
+var bag={
+	brand:"american tourister",
+	color:"grey",
+	material:"polyester",
+	waterproof:"no"
+}
+console.log(bag);
+
+//14
+var sunglasses={
+	idealfor:"men",
+	type:"round",
+	material:"polycarbonate",
+	size:"small"
+}
+console.log(sunglasses);
+
+//15
+var light={
+	bulbused:"led",
+	material:"iron",
+	power:"220v",
+	shape:"rectangle"
+}
+console.log(light);
+
+//16
+var pendrive={
+	storage:"64gb",
+	material:"metal",
+	type:"usb2.0",
+	price:499
+}
+console.log(pendrive);
+
+//17
+var aircooler={
+	brand:"bajaj",
+	airthrow:"30ft",
+	power:"100w",
+	tankcapacity:"36l"
+}
+console.log(aircooler);
+
+//18
+var refrigerator={
+	brand:"whirlpool",
+	material:"alpha steel",
+	type:"triple door",
+	capacity:"250l"
+}
+console.log(refrigerator);
+
+//19
+var bike={
+	brand:"yamaha",
+	model:"mt15",
+	type:"sports",
+	color:"black"
+}
+console.log(bike);
+
+//20
+var myfamily={
+	father:"kandasamy",
+	mother:"thamilasi",
+	sister:"maheshwari"
+}
+console.log(myfamily);
+//21
+var mybio={
+	name:"manikandan",
+	age:24,
+	qualification:"engineering",
+	district:"salem"
+}
+console.log(mybio);
+//22
+
+var juice={
+	flovor:"lemon",
+	quantity:"500ml",
+	vitamin:"c",
+	fat:0
+}
+console.log(juice);
+//23 
+var saree={
+	material:"silk",
+	price:5000,
+	color:"blue"
+}
+console.log(saree);
+//24
+var shirt={
+	brand:"otto",
+	material:"cotton",
+	price:700
+}
+//25
+var remotecar={
+	weight:"500g",
+	control:"remote",
+	type:"chargable",
+	batter:"1000mAh"
+}
+console.log(remotecar);
